7ae1ce4d2758089b7b6b67293c6b617c0ec0eb72
Tuan-Dang05 <85606113+Tuan-Dang05@users.noreply.github.com>
2024-03-26 22:42:05 +0700

    Update main.jsx

--- src/HOME/MainContent/main.jsx
+++ src/HOME/MainContent/main.jsx
@@ -13,8 +13,7 @@ class Main extends Component {
                 <video className="object-fit-cover" width={'102%'} autoPlay muted loop>
                     <source src='./video/bg2.mp4' type='video/mp4'></source>
                 </video>
-                <div data-aos="zoom-out"
-        data-aos-duration="1000" className='nha_que text-center text-light' style={{position:'absolute',top:'40rem',left:'50%',transform:'translate(-50%,-20rem)'}}><p>NHÀ QUÊ ĐI CODE</p></div>
+                <div className='nha_que text-center text-light' style={{position:'absolute',top:'40rem',left:'50%',transform:'translate(-50%,-20rem)'}}><p>NHÀ QUÊ ĐI CODE</p></div>
                 <div className='text-light hoi_anh_em' style={{position:'absolute',top:'50rem',left:'50%',transform:'translate(-50%,-20rem)',fontSize:'30px'}}><p>Hội anh em nhà quê lên phố</p></div>
                 <button className='comic-button' type='button' style={{position:'absolute',top:'70rem',left:'50%',transform:'translate(-50%,-19rem)'}}>CHECK INFO</button>
             </div>
